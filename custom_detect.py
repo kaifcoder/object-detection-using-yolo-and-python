8aaa4df527e9a09c8bce51c90b234a1c4f7279ae
Mohd Kaif <kaifmohd2014@gmail.com>
2022-08-20 22:29:30 +0530

    snake game bsaed on opencv

--- custom_detect.py
+++ custom_detect.py
@@ -1,5 +1,30 @@
 import detect
-opt = {"--source": 0, "--classes": 0}
-print(type(opt))
-print(opt.keys())
-# detect.main(opt)
+print('''
+source -> 
+
+0 # webcam
+img.jpg        # image
+vid.mp4        # video
+path/          # directory
+path/*.jpg     # glob
+'https://youtu.be/Zgi9g1ksQHc'  # YouTube
+'rtsp://example.com/media.mp4'  # RTSP, RTMP, HTTP stream
+''')
+s = input('enter source = ')
+t = int(input('''
+Enter option =>
+1. Count People
+2. Traffic
+'''))
+if t == 2:
+    detect.run(source=s,
+               classes=[1, 2, 3, 5], line_thickness=1, conf_thres=0.25)
+if t == 1:
+    l = input('''want to enter any limit ? y/n''')
+    if l == "Y" or l == "y":
+        lim = int(input("enter the limit => "))
+        detect.run(source=s,
+                   classes=[0], line_thickness=1, conf_thres=0.25, limitpeople=lim)
+    else:
+        detect.run(source=s,
+                   classes=[0], line_thickness=1, conf_thres=0.25)
